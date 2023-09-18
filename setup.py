59260054ac5f0728a75f3bb6935dbc731ac3712c
Chris Williams <christopherlwilliams25@gmail.com>
2023-09-17 21:57:51 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from aws_cdk import core
-
-from excel_data_processor.excel_data_processor_stack import ExcelDataProcessorStack
-
-app = core.App()
-ExcelDataProcessorStack(app, "ExcelDataProcessorStack")
-app.synth()
